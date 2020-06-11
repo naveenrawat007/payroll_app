c36a55da12462d4771a48f5460b90fc15370cbb6
naveen rawat <naveenrawat808@gmail.com>
2020-06-11 23:35:55 +0530

    upload excel file and save it to database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,7 @@ class HomeController < ApplicationController
       payroll = Payrol.find_or_create_by(month: sheet.row(i)[0])
       employee = Employee.find_or_create_by(emp_id: sheet.row(i)[2])
       employee.update(name:sheet.row(i)[1], designation: sheet.row(i)[3], department: sheet.row(i)[4], account_no: sheet.row(i)[6], pf_no: sheet.row(i)[8], pan_no: sheet.row(i)[7], payment_by: sheet.row(i)[5])
+      employee.salary_details.create(payrol_id: payroll.id, working_days: sheet.row(i)[9], pf: sheet.row(i)[10], basic: sheet.row(i)[11], hra: sheet.row(i)[12], incentives: sheet.row(i)[13], lic: sheet.row(i)[15], total_earning: sheet.row(i)[16], pf_deduction: sheet.row(i)[17], esi_deduction: sheet.row(i)[18], pt_deduction: sheet.row(i)[19], tds_deduction: sheet.row(i)[20], total_deduction: sheet.row(i)[21], total_paid_amount: sheet.row(i)[22])
     end
   end
 
