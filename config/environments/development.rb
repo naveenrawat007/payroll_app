db80fd4ba3bd0f5de309adafd1e333f9598e0135
naveen rawat <naveenrawat808@gmail.com>
2020-06-12 16:47:43 +0530

    configure mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -43,6 +43,19 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.default :charset => "utf-8"
+   config.action_mailer.smtp_settings = {
+   address: "smtp.gmail.com",
+   port: 587,
+   domain: "example.com",
+   authentication: "plain",
+   enable_starttls_auto: true,
+   user_name: "*******" ,
+   password: "*****"
+  }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
