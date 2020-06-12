adba63e48ecd99c8f3a22acfe2aad939d94d3724
naveen rawat <naveenrawat808@gmail.com>
2020-06-12 13:41:47 +0530

    create routes and style pdf and add gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "payrol#index"
   post '/import_data' => "payrol#generate_data"
   get '/show' => 'payrol#show'
+  get '/download_slip/:id' => "payrol#download_slip"
 end
