f578fa43d9b9206137304b3bb20d2f1bff3b114e
naveen rawat <naveenrawat808@gmail.com>
2020-06-13 08:26:33 +0530

    config redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+uri = URI.parse('redis://127.0.0.1:6379 ')
+REDIS = Redis.new(:url => uri)
